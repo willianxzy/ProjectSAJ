dd2a83d7f7233e58acfd7cd88c27534d331d566d
WillianKuhnLittig <willianlittig14@gmail.com>
2024-10-30 14:27:28 -0300

    feat: backend projeto p/ deploy

---
+++ src/main/java/br/com/itilh/bdpedidos/sistemapedidos/controller/MunicipioController.java
@@ -0,0 +1,63 @@
+package br.com.itilh.bdpedidos.sistemapedidos.controller;
+
+import java.math.BigInteger;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.itilh.bdpedidos.sistemapedidos.model.Municipio;
+import br.com.itilh.bdpedidos.sistemapedidos.service.MunicipioService;
+
+
+
+@RestController
+public class MunicipioController {
+
+    @Autowired
+    private MunicipioService service;
+
+   
+    @GetMapping("/municipios")
+    public List<Municipio> getMunicipios() {
+        return service.listarMunicipios();
+    }
+
+    @GetMapping("/municipios/estado-id/{id}")
+    public List<Municipio> getMunicipiosPorEstadoId(@PathVariable BigInteger id) {
+        return service.listarMunicipiosPorEstadoId(id);
+    }
+
+    @GetMapping("/municipios/estado-nome/{nome}")
+    public List<Municipio> getMunicipiosPorEstadoNome(@PathVariable String nome) {
+        return service.listarMunicipiosPorEstadoNome(nome);
+    }
+
+    @GetMapping("/municipio/{id}")
+    public Municipio getMunicipioPorId(@PathVariable BigInteger id) throws Exception {
+        return service.buscarMunicipioPorId(id);
+    }
+
+    @PostMapping("/municipio")
+    public Municipio postMunicipio(@RequestBody Municipio entity) throws Exception {    
+        return service.criarMunicipio(entity);
+    }
+
+    @PutMapping("/municipio/{id}")
+    public Municipio putMunicipio(@PathVariable BigInteger id, @RequestBody Municipio entity) throws Exception {
+        return service.alterarMunicipio(id, entity);
+    }
+
+    @DeleteMapping("/municipio/{id}")
+    public String deleteMunicipio(@PathVariable BigInteger id) throws Exception{
+        return service.excluirMunicipio(id);
+    }
+    
+    
+}
